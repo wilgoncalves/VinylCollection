7df4db9f2dc8212a838864ff0773cfde043b71a6
Willian Gonçalves <santoswil81@gmail.com>
2024-11-21 15:06:07 -0300

    Connecting with database

--- VinylCollection/Program.cs
+++ VinylCollection/Program.cs
@@ -1,9 +1,39 @@
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do Serilog para logar em um arquivo
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+
+// Registrar a conexão como um serviço singleton (abre a conexão uma vez)
+builder.Services.AddSingleton<IDbConnection>(sp => new MySqlConnection(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var connection = app.Services.GetRequiredService<IDbConnection>())
+{
+    try
+    {
+        // Tenta abrir a conexão com o banco de dados
+        connection.Open();
+    }
+    catch (Exception ex)
+    {
+        // Logando o erro usando o Serilog
+        Log.Error($"[{DateTime.Now}] Erro ao conectar ao banco de dados: {ex.Message}");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
